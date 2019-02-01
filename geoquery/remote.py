295e8e585e8aa47f1a4bd0a5e61f060a5f17f4fd
ianertson <sebbekarlsson97@gmail.com>
2019-02-01 12:51:14 +0100

    cache country files

--- geoquery/remote.py
+++ geoquery/remote.py
@@ -1,10 +1,21 @@
 from StringIO import StringIO
 from zipfile import ZipFile
 from urllib import urlopen
+import os
 
 
-def download_and_read(country_code):
-    return ZipFile(StringIO(urlopen(
+def download_and_read(country_code, use_cache=True):
+    fname = country_code.upper() + '.txt'
+    cachepath = '/tmp/' + fname
+
+    if use_cache and os.path.isfile(cachepath):
+        return open(cachepath).readlines()
+
+    lines = ZipFile(StringIO(urlopen(
         'http://download.geonames.org/export/dump/{}.zip'
         .format(country_code.upper())
-    ).read())).open(country_code.upper() + '.txt').readlines()
+    ).read())).open(fname).readlines()
+
+    open(cachepath, 'w+').writelines(lines)
+
+    return lines
